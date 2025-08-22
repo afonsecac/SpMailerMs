8e6f68a37a82832b4373f9ee638ecdd06108beb6
Alexander Fonseca <alexander.afonsecac@gmail.com>
2025-08-23 01:46:33 +0200

    Configs in ms

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,19 @@ import * as joi from 'joi';
 
 interface EnvVars {
   MS_PORT: number;
+  MAILER_HOST: string;
+  MAILER_PORT: number;
+  MAILER_USER: string;
+  MAILER_PASS: string;
 }
 
 const envVarsSchema = joi
   .object({
     MS_PORT: joi.number().default(3002),
+    MAILER_HOST: joi.string().required(),
+    MAILER_PORT: joi.number().required(),
+    MAILER_USER: joi.string().required(),
+    MAILER_PASS: joi.string().required(),
   })
   .unknown(true);
 
@@ -21,4 +29,8 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.MS_PORT,
+  mailerHost: envVars.MAILER_HOST,
+  mailerPort: envVars.MAILER_PORT,
+  mailerUser: envVars.MAILER_USER,
+  mailerPass: envVars.MAILER_PASS,
 };
