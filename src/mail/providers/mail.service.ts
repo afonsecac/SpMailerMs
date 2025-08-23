3e2efef9c6eefdfbd19c63edb040ad39ccc03040
Alexander Fonseca <alexander.afonsecac@gmail.com>
2025-08-23 15:16:41 +0200

    Update mailer service

--- src/mail/providers/mail.service.ts
+++ src/mail/providers/mail.service.ts
@@ -9,20 +9,23 @@ export class MailService {
   async sendRegisterMail(account: CreateMailDto) {
     const logger = new Logger('Mailer-Microservice');
     try {
-      await this.mailerService.sendMail({
-        to: account.email,
-        subject: account.subject,
-        template: `./register.${account.platform.toLowerCase()}`,
-        context: {
-          name: account.name,
-          code: account.code,
-          url: account.url,
-          platform: account.platform,
-        },
-      });
-      logger.log(
-        `Mail register sent with template: register.${account.platform}.hbs to account: ${account.email}`,
-      );
+      await this.mailerService
+        .sendMail({
+          to: account.email,
+          subject: account.subject,
+          template: `./register.${account.platform.toLowerCase()}`,
+          context: {
+            name: account.name,
+            code: account.code,
+            url: account.url,
+            platform: account.platform,
+          },
+        })
+        .then(() => {
+          logger.log(
+            `Mail register sent with template: register.${account.platform}.hbs to account: ${account.email}`,
+          );
+        });
     } catch (error) {
       logger.error(error);
     }
